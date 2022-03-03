c65776e38285b923cd4ddf571e2d4352db66cc66
wenyihong <787252975wyh@gmail.com>
2022-03-03 09:51:07 +0800

    leetcode 简化路径94 二叉树的中序遍历

---
+++ src/main/java/zyj/yihong/leetcode/simple/stack/InorderTraversal94.java
@@ -0,0 +1,67 @@
+package zyj.yihong.leetcode.simple.stack;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+import java.util.TreeMap;
+
+/**
+ * 给定一个二叉树的根节点 root ，返回它的 中序 遍历。
+ *
+ */
+public class InorderTraversal94 {
+
+    private List<Integer> recursionList = new ArrayList<>();
+
+    /**
+     * 使用栈的方法
+     * @param root
+     * @return
+     */
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> retArray = new ArrayList<>();
+        Stack<TreeNode> nodeStack = new Stack<>();
+        TreeNode cur = root;
+        while(cur!=null || nodeStack.size()>0){
+            while (cur!=null){
+                nodeStack.push(cur);
+                cur = cur.left;
+            }
+
+            if (nodeStack.size()>0){
+                TreeNode pop = nodeStack.pop();
+                retArray.add(pop.val);
+                cur = pop.right;
+            }
+        }
+        return retArray;
+    }
+
+    public List<Integer> inorderTraversal2(TreeNode root) {
+        inorderTraversalRecursion(root);
+        return recursionList;
+    }
+
+
+    public void inorderTraversalRecursion(TreeNode root) {
+        if (root==null){
+            return;
+        }
+        inorderTraversalRecursion(root.left);
+        recursionList.add(root.val);
+        inorderTraversalRecursion(root.right);
+    }
+
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+}
