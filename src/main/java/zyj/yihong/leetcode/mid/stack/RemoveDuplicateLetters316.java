7e084169046111734c5fda5b9125fb2442826687
wenyihong <787252975wyh@gmail.com>
2022-03-10 11:17:27 +0800

    leetcode 316. 去除重复字母 单调栈

--- src/main/java/zyj/yihong/leetcode/mid/stack/RemoveDuplicateLetters316.java
+++ src/main/java/zyj/yihong/leetcode/mid/stack/RemoveDuplicateLetters316.java
@@ -1,39 +1,60 @@
 package zyj.yihong.leetcode.mid.stack;
 
+import java.util.Stack;
+
 /**
  * 316. 去除重复字母
  * 给你一个字符串 s ，请你去除字符串中重复的字母，使得每个字母只出现一次。需保证 返回结果的字典序最小（要求不能打乱其他字符的相对位置）
  */
 public class RemoveDuplicateLetters316 {
+
+    /**
+     * 单调栈
+     *
+     * @param s
+     * @return
+     */
     public static String removeDuplicateLetters(String s) {
-        boolean[] vis = new boolean[26];
-        int[] num = new int[26];
+        Stack<Character> stack = new Stack<>();
+        int[] charNum = new int[26];
+        boolean[] charInStack = new boolean[26];
+
+        // 计算某个字符存在的个数
         for (int i = 0; i < s.length(); i++) {
-            num[s.charAt(i) - 'a']++;
+            char c = s.charAt(i);
+            charNum[c - 'a']++;
         }
 
-        StringBuffer sb = new StringBuffer();
         for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (!vis[ch - 'a']) {
-                while (sb.length() > 0 && sb.charAt(sb.length() - 1) > ch) {
-                    if (num[sb.charAt(sb.length() - 1) - 'a'] > 0) {
-                        vis[sb.charAt(sb.length() - 1) - 'a'] = false;
-                        sb.deleteCharAt(sb.length() - 1);
-                    } else {
-                        break;
-                    }
-                }
-                vis[ch - 'a'] = true;
-                sb.append(ch);
+            char c = s.charAt(i);
+            if (charInStack[c - 'a']) {
+                charNum[c-'a']--;
+                continue;
             }
-            num[ch - 'a'] -= 1;
+
+            while (stack.size() != 0) {
+                Character peek = stack.peek();
+                if (c - peek < 0 && charNum[peek - 'a'] != 0) {
+                    stack.pop();
+                    charInStack[peek - 'a'] = false;
+                    continue;
+                }
+                break;
+            }
+            stack.push(c);
+            charInStack[c - 'a'] = true;
+            charNum[c - 'a']--;
+
         }
-        return sb.toString();
+        StringBuilder stringBuilder = new StringBuilder(stack.size());
+        while (stack.size() != 0) {
+            stringBuilder.append(stack.pop());
+        }
+        return stringBuilder.reverse().toString();
     }
 
     public static void main(String[] args) {
-        String s = "bcabc";
+        String s = "bbcaac";
         String s1 = removeDuplicateLetters(s);
         System.out.println(s1);
     }
