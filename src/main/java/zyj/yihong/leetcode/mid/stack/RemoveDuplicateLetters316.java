ce0352cdda5e216b045f08695bda29446be1a23d
wenyihong <787252975wyh@gmail.com>
2022-03-10 10:18:25 +0800

    leetcode  402. 移掉 K 位数字 (单调栈)

---
+++ src/main/java/zyj/yihong/leetcode/mid/stack/RemoveDuplicateLetters316.java
@@ -0,0 +1,40 @@
+package zyj.yihong.leetcode.mid.stack;
+
+/**
+ * 316. 去除重复字母
+ * 给你一个字符串 s ，请你去除字符串中重复的字母，使得每个字母只出现一次。需保证 返回结果的字典序最小（要求不能打乱其他字符的相对位置）
+ */
+public class RemoveDuplicateLetters316 {
+    public static String removeDuplicateLetters(String s) {
+        boolean[] vis = new boolean[26];
+        int[] num = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            num[s.charAt(i) - 'a']++;
+        }
+
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (!vis[ch - 'a']) {
+                while (sb.length() > 0 && sb.charAt(sb.length() - 1) > ch) {
+                    if (num[sb.charAt(sb.length() - 1) - 'a'] > 0) {
+                        vis[sb.charAt(sb.length() - 1) - 'a'] = false;
+                        sb.deleteCharAt(sb.length() - 1);
+                    } else {
+                        break;
+                    }
+                }
+                vis[ch - 'a'] = true;
+                sb.append(ch);
+            }
+            num[ch - 'a'] -= 1;
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String s = "bcabc";
+        String s1 = removeDuplicateLetters(s);
+        System.out.println(s1);
+    }
+}
