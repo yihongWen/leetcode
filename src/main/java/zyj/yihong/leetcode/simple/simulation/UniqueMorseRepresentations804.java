7625e2150cf001a006cf7c9fd1d40e0588adc252
wenyihong <787252975wyh@gmail.com>
2022-04-11 13:09:44 +0800

    804. 唯一摩尔斯密码词

---
+++ src/main/java/zyj/yihong/leetcode/simple/simulation/UniqueMorseRepresentations804.java
@@ -0,0 +1,33 @@
+package zyj.yihong.leetcode.simple.simulation;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 804. 唯一摩尔斯密码词
+ * 国际摩尔斯密码定义一种标准编码方式，将每个字母对应于一个由一系列点和短线组成的字符串， 比如:
+ *
+ * 'a' 对应 ".-" ，
+ * 'b' 对应 "-..." ，
+ * 'c' 对应 "-.-." ，以此类推。
+ * 为了方便，所有 26 个英文字母的摩尔斯密码表如下：
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/unique-morse-code-words
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class UniqueMorseRepresentations804 {
+    public int uniqueMorseRepresentations(String[] words) {
+        String[] info = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        Set<String> set = new HashSet<>();
+        for (String word : words) {
+            String curAns = "";
+            for (int i = 0; i < word.length(); i++) {
+                curAns += info[word.charAt(i) - 'a'];
+            }
+            set.add(curAns);
+        }
+
+        return set.size();
+    }
+}
