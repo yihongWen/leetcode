f886d38e39ae98fddd5a79553afe03b1f41cb232
wenyihong <787252975wyh@gmail.com>
2022-05-25 10:00:44 +0800

    304. 二维区域和检索 - 矩阵不可变（二维前缀和）

---
+++ src/main/java/zyj/yihong/leetcode/mid/arr/NumMatrix304.java
@@ -0,0 +1,60 @@
+package zyj.yihong.leetcode.mid.arr;
+
+import com.google.gson.Gson;
+
+/**
+ * 304. 二维区域和检索 - 矩阵不可变
+ * 给定一个二维矩阵 matrix，以下类型的多个请求：
+ * <p>
+ * 计算其子矩形范围内元素的总和，该子矩阵的 左上角 为 (row1, col1) ，右下角 为 (row2, col2) 。
+ * 实现 NumMatrix 类：
+ * <p>
+ * NumMatrix(int[][] matrix) 给定整数矩阵 matrix 进行初始化
+ * int sumRegion(int row1, int col1, int row2, int col2) 返回 左上角 (row1, col1) 、右下角 (row2, col2) 所描述的子矩阵的元素 总和 。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/range-sum-query-2d-immutable
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class NumMatrix304 {
+    // 二维前缀和
+    int[][] preSum;
+    int[][] sums;
+
+    public NumMatrix304(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        preSum = new int[m + 1][n + 1];
+
+        // 初始化前缀和
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                preSum[i][j] = preSum[i - 1][j] + preSum[i][j - 1] - preSum[i - 1][j - 1] + matrix[i - 1][j - 1];
+            }
+        }
+
+        sums = new int[m + 1][n + 1];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                sums[i + 1][j + 1] = sums[i][j + 1] + sums[i + 1][j] - sums[i][j] + matrix[i][j];
+            }
+        }
+    }
+
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+//        return sums[row2 + 1][col2 + 1] - sums[row1][col2 + 1] - sums[row2 + 1][col1] + sums[row1][col1];
+        return preSum[row2 + 1][col2 + 1] - preSum[row1][col2 + 1] - preSum[row2 + 1][col1] + preSum[row1][row2];
+    }
+
+    public static void main(String[] args) {
+        String s = "[[3,0,1,4,2],[5,6,3,2,1],[1,2,0,1,5],[4,1,0,1,7],[1,0,3,0,5]]";
+        Gson gson = new Gson();
+        int[][] arr = gson.fromJson(s, int[][].class);
+        NumMatrix304 numMatrix304 = new NumMatrix304(arr);
+        int i = numMatrix304.sumRegion(2, 1, 4, 3);
+        System.out.println(i);
+
+
+    }
+}
