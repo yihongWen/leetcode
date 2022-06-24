a849a9c8f5ae44e89442a63d5bb1713ec7ec69b0
wenyihong <787252975wyh@gmail.com>
2022-06-24 17:33:37 +0800

    450. 删除二叉搜索树中的节点（删除二叉树的节点,使用迭代的方式，存储父节点的关系）

--- src/main/java/zyj/yihong/leetcode/special/top/recursion/DeleteNode_M_450.java
+++ src/main/java/zyj/yihong/leetcode/special/top/recursion/DeleteNode_M_450.java
@@ -2,6 +2,8 @@ package zyj.yihong.leetcode.special.top.recursion;
 
 import zyj.yihong.leetcode.utils.TreeNode;
 
+import java.util.Objects;
+
 /**
  * 450. 删除二叉搜索树中的节点
  */
@@ -57,5 +59,73 @@ public class DeleteNode_M_450 {
         successorNode.right = root.right;
         return successorNode;
 
+    }
+
+
+    public TreeNode deleteNodeOpt(TreeNode root, int key) {
+        // 使用迭代的方式删除二叉树节点，保存后继节点的父节点，将其父节点的左节点指向后继节点的右节点
+
+        // 先找到要删除的node
+        TreeNode curNode = root;
+        TreeNode curNodeParent = null;
+        while (curNode!=null&&curNode.val!=key){
+            curNodeParent = curNode;
+            if (curNode.val>key){
+                curNode = curNode.left;
+                continue;
+            }
+            curNode = curNode.right;
+        }
+
+        boolean leftJoin = false;
+        if (curNodeParent!=null&& Objects.equals(curNodeParent.left, curNode)){
+            leftJoin = true;
+        }
+
+        // 判断删除节点的子树情况:
+
+        // 1. 不存在key
+        if (curNode==null){
+            return root;
+        }
+
+        // 1、删除的节点左右孩子都为空的情况
+        if (curNode.left==null&&curNode.right==null){
+            curNode = null;
+        }else if (curNode.left==null){
+            curNode = curNode.right;
+        }else if (curNode.right==null){
+            curNode = curNode.left;
+        }else {
+            // 左右孩子,寻找后继
+            TreeNode successorNodeParent = curNode;
+            TreeNode successorNode = curNode.right;
+            while (successorNode.left != null) {
+                successorNodeParent = successorNode;
+                successorNode = successorNode.left;
+
+            }
+            // 后继的parent的左节点执行后继节点的右节点
+            if (!Objects.equals(successorNodeParent, curNode)) {
+                successorNodeParent.left = successorNode.right;
+            }else {
+                successorNodeParent.right = successorNode.right;
+            }
+
+            successorNode.left = curNode.left;
+            successorNode.right = curNode.right;
+            curNode = successorNode;
+        }
+
+        if (curNodeParent==null){
+            return curNode;
+        }else {
+            if (leftJoin) {
+                curNodeParent.left = curNode;
+            } else {
+                curNodeParent.right = curNode;
+            }
+            return root;
+        }
     }
 }
